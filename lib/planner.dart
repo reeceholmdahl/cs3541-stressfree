d66cf3d6800f8f808744b124fbf4faaa0a584e63
Reece Holmdahl <holmd353@umn.edu>
2022-03-23 17:50:47 -0500

    Working on tracking daily activities feature Making a text entry field in the ActivityTracker class Making ActivityTracker a stateful widget Treating activity entry as a form for validation purposes Using 'https://api.flutter.dev/flutter/widgets/Form-class.html' as reference

--- lib/planner.dart
+++ lib/planner.dart
@@ -1,10 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class planner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ActivityTracker()
+      ],
+    )
+  }
+}
+
+class ActivityTracker extends StatefulWidget {
+  const ActivityTracker({Key? key}) : super(key: key);
+
+  @override
+  State<ActivityTracker> createState() => _ActivityTrackerState();
+}
+
+class _ActivityTrackerState extends State<ActivityTracker> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Row(
+        children: [
+          TextFormField()
+        ],
+      )
+    )
   }
 }
