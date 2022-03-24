4afcfa019a45562261f789a7dae1fc55436e6e83
Miles Kuhn <kuhn0192@d.umn.edu>
2022-03-23 19:25:45 -0500

    Added basic alarm functionality with pop up box

--- lib/Alarms/alarm.dart
+++ lib/Alarms/alarm.dart
@@ -2,7 +2,7 @@ import 'package:firstapp/Alarms/createNewAlarm.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'dart:core';
-
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,102 +18,118 @@ class Alarms extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Alarms',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Alarm Page'),
-        ),
-        body: Center(
+      home: Test(),
+    );
+  }
+}
 
-          child: Column(
-              mainAxisSize: MainAxisSize.max,
+class Test extends StatelessWidget {
+  buildNewAlarmPopup(BuildContext context) {
+    TextEditingController hourController = TextEditingController();
+    TextEditingController minuteController = TextEditingController();
 
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("New Alarm"),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 30), //adjust heights
-                AlarmIcon(),
-                NewAlarmButton()
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.yellow,
+                    //borderRadius: BorderRadius.circular(11)
+                  ),
+                  child: Center(
+                    child: TextField(
+                      controller: hourController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: minuteController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
               ],
-          ),
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                  elevation: 5.0, child: Text("Submit"),
+                  onPressed: () {
+                    int hour;
+                    int minutes;
+                    hour = int.parse(hourController.text);
+                    minutes = int.parse(minuteController.text);
 
+                    // creating alarm after converting hour
+                    // and minute into integer
+                    FlutterAlarmClock.createAlarm(hour, minutes);
+                 }
+              )
+
+            ],
+          );
+        }); //showDialog
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alarm Page'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(height: 30), //adjust heights
+            AlarmIcon(),
+            TextButton(
+              child: const Text(
+                'AlarmButton',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {
+                buildNewAlarmPopup(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
-
 }
 
 class AlarmIcon extends StatelessWidget {
   const AlarmIcon({Key? key}) : super(key: key);
 
- Widget build(BuildContext context) {
-   return Row(
-     mainAxisAlignment: MainAxisAlignment.center,
-     children: const <Widget>[
-       Icon(
-         Icons.access_alarms,
-         color: Colors.pink,
-         size: 100.0,
-         semanticLabel: 'Text to announce in accessibility modes',
-       ),
-
-     ],
-   );
- }
-}
-
-class NewAlarmButton extends StatefulWidget {
-  const NewAlarmButton({Key? key}) : super(key: key);
-
-  @override
-  State<NewAlarmButton> createState() => _NewAlarmButtonState();
-}
-
-class _NewAlarmButtonState extends State<NewAlarmButton> {
-  @override
   Widget build(BuildContext context) {
-    final ButtonStyle style =
-    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-
-
-          const SizedBox(height: 30),
-          buildNewAlarmPopup()
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const <Widget>[
+        Icon(
+          Icons.access_alarms,
+          color: Colors.pink,
+          size: 100.0,
+          semanticLabel: 'Text to announce in accessibility modes',
+        ),
+      ],
     );
   }
-
-  Container buildNewAlarmPopup() {
-    return Container(
-      margin: const EdgeInsets.all(25),
-      child: TextButton(
-          child: const Text(
-            'Create timer',
-            style: TextStyle(fontSize: 20.0),
-          ),
-          onPressed: () {
-            // create timer
-
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return AboutDialog(
-                    children: [
-                      Center(
-                        child: Text("New Alarm",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold)),
-                      )
-                    ],
-                  );
-                });
-          }),
-    );
-  }
-
 }
-
-
