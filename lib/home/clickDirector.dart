0a5425b54811e466fa69cce8511efd261402a0fd
JoeWhite <whit3261@d.umn.edu>
2022-03-28 17:20:26 -0500

    add drawer.dart, and journal.

--- lib/home/clickDirector.dart
+++ lib/home/clickDirector.dart
@@ -2,22 +2,25 @@
 
 import 'package:flutter/cupertino.dart';
 
+import '../main.dart';
+import 'home.dart';
+
 class clickDirector extends StatelessWidget {
   const clickDirector({
     required this.id,
-    required this.name,
-    required this.color,
     Key? key,
   }) : super(key: key);
 
   final int id;
-  final String name;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    //Can just direct to which page based on provided info
-    throw UnimplementedError();
+    if(this.id == 0){
+      return homePage(); //remove these and just replace with your class
+    }else if(this.id == 1){
+      return MyApp();//remove these and just replace with your class
+    }else{
+      return MyApp();//remove these and just replace with your class
+    }
   }
 }
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../drawer.dart';
 import 'homeTab.dart';
 
 class homePage extends StatelessWidget {
@@ -30,95 +31,6 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-
-class sideDrawerLeft extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-      return Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-
-              decoration: BoxDecoration(
-
-                color: const Color(0xff7c94b6),
-                image: const DecorationImage(
-                  image: NetworkImage('https://cdn.sixtyandme.com/wp-content/uploads/2020/12/iStock-848645812-scaled.jpg'),
-                  fit: BoxFit.cover,
-                ),
-
-                //color: Colors.blue,
-                //alignment: Alignment.center,
-              ),
-
-              child: Text(
-                  ' ',
-                  style: const TextStyle(
-                    fontSize: 45,
-                  ),
-              ),
-              margin: EdgeInsets.all(0),
-            ),
-            ListTile(
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push<void>(context,
-                    MaterialPageRoute(builder: (context) => const homePage()));
-              },
-            ),
-            ListTile(
-              title: const Text('Self Care Journal'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Stress Release Videos'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Stress Management Techniques'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Self Care Ideas'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('More Soothing Music'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      );
-  }
-}
 
 
 
