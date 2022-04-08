53ed636fbd438839a3a611543345574d096415fd
Reece Holmdahl <holmd353@umn.edu>
2022-04-07 22:41:44 -0500

    Updating mood data type for hopefully the last time to include vectorized icon data

--- lib/data/mood.dart
+++ lib/data/mood.dart
@@ -4,24 +4,26 @@ import 'package:flutter/material.dart';
 class Mood {
   final String name;
   final MaterialColor color;
-  final ImageIcon icon;
+  final IconData iconData;
 
-  const Mood(this.name, this.color, this.icon);
+  const Mood(this.name, this.color, this.iconData);
 
   /// Moods for now, subject to change.
   /// Using a map for data types like this so they can be referenced back by just their name
   static final moods = {
-    'bad':
-        Mood('Bad', Colors.red, ImageIcon(AssetImage('assets/icon-bad.png'))),
-    'medium': Mood('Medium', Colors.amber,
-        ImageIcon(AssetImage('assets/icon-medium.png'))),
-    'good': Mood(
-        'Good', Colors.lime, ImageIcon(AssetImage('assets/icon-good.png'))),
-    'great': Mood(
-        'Great', Colors.green, ImageIcon(AssetImage('assets/icon-great.png')))
+    'bad': Mood('Bad', Colors.red, icon_bad),
+    'medium': Mood('Medium', Colors.amber, icon_medium),
+    'good': Mood('Good', Colors.lime, icon_good),
+    'great': Mood('Great', Colors.green, icon_great)
   };
 
   /// Null object design pattern
-  static final nullMood = Mood(
-      'Null', Colors.brown, ImageIcon(AssetImage('assets/transparent_file')));
+  static final nullMood = Mood('Null', Colors.grey, IconData(0x1));
+
+  static const String _fontFamily = 'icomoon';
+
+  static const IconData icon_bad = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData icon_medium = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData icon_good = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData icon_great = IconData(0xe903, fontFamily: _fontFamily);
 }
