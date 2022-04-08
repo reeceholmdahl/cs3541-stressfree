ae2c2ebf59af63a4c12151e67932212590f81817
Reece Holmdahl <holmd353@umn.edu>
2022-04-07 22:39:53 -0500

    Working on mood selector for tracking a planned activity Turned mood icon image to icon data using Icomoon app which turns vector images into fonts

--- lib/planner/planner.dart
+++ lib/planner/planner.dart
@@ -7,6 +7,7 @@ import 'package:firstapp/planner/planner_settings.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import '../data/mood.dart';
 import 'expandable_fab.dart';
 
 class _Notifier with ChangeNotifier {
@@ -94,28 +95,27 @@ class PlannerFab extends StatelessWidget {
 }
 
 class ActivityListItem<T extends Activity> extends StatelessWidget {
-  ActivityListItem({
-    Key? key,
-    required this.activity,
-  }) : super(key: key);
+  ActivityListItem({Key? key, required this.activity, required this.gradient})
+      : super(key: key);
 
   final T activity;
+  final Gradient gradient;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      color: activity.category.color.shade100,
+    return DecoratedBox(
+      decoration: BoxDecoration(gradient: gradient),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
         child: SizedBox(
           height: 64,
           child: Material(
             borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: activity.category.color.shade400,
+            color: activity.category.color.shade300,
             child: InkWell(
               borderRadius: const BorderRadius.all(Radius.circular(8)),
-              splashColor: activity.category.color.shade300,
+              splashColor: activity.category.color.shade200,
               onDoubleTap: () {
                 log('Double tap');
               },
@@ -135,30 +135,7 @@ class ActivityListItem<T extends Activity> extends StatelessWidget {
                       log('Complete activity');
                       showDialog(
                           context: context,
-                          builder: (context) => AlertDialog(
-                                title: Column(
-                                  children: [
-                                    Text('Track Activity'),
-                                    SizedBox(height: 12),
-                                    Text(activity.name,
-                                        style: theme.textTheme.subtitle1!
-                                            .copyWith(
-                                                color: activity
-                                                    .category.color.shade800))
-                                  ],
-                                ),
-                                content: Text(activity.name +
-                                    '\n' +
-                                    activity.category.name),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('CANCEL'),
-                                  ),
-                                ],
-                              ));
+                          builder: (context) => MoodRater(activity: activity));
                     },
                   ),
                 ],
@@ -167,6 +144,57 @@ class ActivityListItem<T extends Activity> extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class MoodRater extends StatefulWidget {
+  const MoodRater({
+    Key? key,
+    required this.activity,
+  }) : super(key: key);
+
+  final Activity activity;
+
+  @override
+  State<MoodRater> createState() => _MoodRaterState();
+}
+
+class _MoodRaterState extends State<MoodRater> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return AlertDialog(
+      contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+      title: Column(
+        children: [
+          Text('Track Activity'),
+          SizedBox(height: 12),
+          Text(widget.activity.name,
+              style: theme.textTheme.subtitle1!
+                  .copyWith(color: widget.activity.category.color.shade800))
+        ],
+      ),
+      content: SizedBox(
+        width: double.maxFinite,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          for (var mood in Mood.moods.values)
+            IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  log(mood.name);
+                },
+                icon: Icon(mood.iconData, size: 50))
+        ]),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('CANCEL'),
+        ),
+      ],
     );
   }
 }
@@ -207,13 +235,59 @@ class _ActivityListState extends State<ActivityList> {
                 restorationId: 'activitiesList',
                 onReorder: (oldIndex, newIndex) {
                   final replace = _activities.removeAt(oldIndex);
-                  _activities.insert(math.max(0, newIndex - 1), replace);
+                  if (oldIndex > newIndex) {
+                    _activities.insert(math.max(0, newIndex), replace);
+                  } else {
+                    _activities.insert(newIndex - 1, replace);
+                  }
                 },
                 itemCount: _activities.length,
                 itemBuilder: (context, index) {
                   final activity = _activities[index];
+                  final Color thisColor = activity.category.color.shade100;
+                  final Color? lastColor = (index - 1 >= 0)
+                      ? _activities[index - 1].category.color.shade100
+                      : null;
+                  final Color? nextColor = (index + 1 < _activities.length)
+                      ? _activities[index + 1].category.color.shade100
+                      : null;
+                  LinearGradient gradient;
+
+                  if (index == 0) {
+                    if (_activities.length == 1) {
+                      gradient = LinearGradient(colors: [thisColor]);
+                    } else {
+                      gradient = LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            thisColor,
+                            Color.lerp(thisColor, nextColor, 0.5)!
+                          ]);
+                    }
+                  } else if (index == _activities.length - 1) {
+                    gradient = LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color.lerp(lastColor, thisColor, 0.5)!,
+                          thisColor,
+                        ]);
+                  } else {
+                    gradient = LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color.lerp(lastColor, thisColor, 0.5)!,
+                          thisColor,
+                          Color.lerp(thisColor, nextColor, 0.5)!,
+                        ]);
+                  }
+
                   return ActivityListItem(
-                      key: ValueKey(index), activity: activity);
+                      key: ValueKey(index),
+                      activity: activity,
+                      gradient: gradient);
                 }),
           ),
         ],
