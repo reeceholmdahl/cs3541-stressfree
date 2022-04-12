4452255d20b417fd6f63b5c206951abf882a98a7
Victor LaBrie <vplabrie@gmail.com>
2022-04-12 11:22:56 -0500

    Music player started

---
+++ lib/music/playerWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class musicPlayer extends StatelessWidget{
+  const musicPlayer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Planner')), body: ActivityTracker());
+  }
+}
