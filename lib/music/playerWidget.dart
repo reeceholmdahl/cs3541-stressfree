80a67ded1d9a8f4b1868d7123df8326f6e72c139
Victor LaBrie <vplabrie@gmail.com>
2022-04-12 16:02:03 -0500

    I thought I already committed this.

--- lib/music/playerWidget.dart
+++ lib/music/playerWidget.dart
@@ -6,6 +6,6 @@ class musicPlayer extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Planner')), body: ActivityTracker());
+        appBar: AppBar(title: const Text('Planner')), body: );
   }
 }
