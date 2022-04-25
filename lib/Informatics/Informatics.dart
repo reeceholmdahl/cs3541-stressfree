5ef89426272ccf11167d42bf9ec560d45f036f27
Miles Kuhn <kuhn0192@d.umn.edu>
2022-04-25 18:57:16 -0500

    Updated chart

--- lib/Informatics/Informatics.dart
+++ lib/Informatics/Informatics.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
 import 'dart:math' as math;
 import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:firstapp/data/mood.dart';
-
 import 'package:pie_chart/pie_chart.dart';
+
+import '../drawer.dart';
 
 class Informatics extends StatelessWidget {
   @override
@@ -21,7 +24,7 @@ class Informatics extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
         brightness: Brightness.dark,
       ),
-      home: HomePage(storage: CounterStorage()),
+      home: HomePage(),
     );
   }
 }
@@ -29,9 +32,7 @@ class Informatics extends StatelessWidget {
 enum LegendShape { Circle, Rectangle }
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.storage}) : super(key: key);
-
-  final CounterStorage storage;
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -39,11 +40,35 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final dataMap = <String, double>{
-    "Bad": 5,
-    "Medium": 3,
-    "Good": 2,
-    "Great": 2,
+    "Happy": 5,
+    "Mildly Happy": 3,
+    "Mildly Sad": 2,
+    "Sad": 2,
   };
+
+  Future<String> pullData() async {
+    final ref = FirebaseDatabase.instance.ref('Planner');
+    DatabaseEvent event = await ref.once();
+
+    String myString = event.snapshot.value.toString();
+
+    return myString;
+    //return event.snapshot.children.first.toString();
+  }
+
+
+  String insertChar(String endString, int pos, String toInsert) {
+    endString = endString.substring(0, pos + 1) +
+        toInsert +
+        endString.substring(pos + 1);
+    return endString;
+  }
+
+  static String skipOver(String endString, int pos, int newPos) {
+    endString =
+        endString.substring(0, pos + 1) + endString.substring(newPos + 1);
+    return endString;
+  }
 
   final colorList = <Color>[
     (Mood.moods["bad"]!.color),
@@ -101,141 +126,187 @@ class _HomePageState extends State<HomePage> {
           ]);
         }); //showDialog
   }
-
   @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>['A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100];
-    final List<String> activityList = <String>['A', 'B', 'C'];
 
-    String myString = "";
 
     @override
     void initState() {
       super.initState();
-      widget.storage.readCounter().then((String value) {
-        setState(() {
-          myString = value;
-        });
-      });
     }
 
     //do action, maybe not needed for this application
 
-    Future<File> _incrementCounter(String value) {
-      setState(() {
-        myString = value;
-      });
-
-      // Write the variable as a string to the file.
-      return widget.storage.writeCounter(myString);
-    }
-
     //final dataMap = readDataFromFile();
 
-    final chart = PieChart(
-      key: ValueKey(key),
-      dataMap: dataMap,
-      animationDuration: Duration(milliseconds: 800),
-      chartLegendSpacing: _chartLegendSpacing!,
-      chartRadius: math.min(MediaQuery.of(context).size.width / 2, 400),
-      colorList: colorList,
-      initialAngleInDegree: 0,
-      chartType: _chartType!,
-
-
-      // legendLabels: _showLegendLabel ? legendLabels : {},
-
-      chartValuesOptions: ChartValuesOptions(
-        showChartValueBackground: _showChartValueBackground,
-        showChartValues: _showChartValues,
-        showChartValuesInPercentage: _showChartValuesInPercentage,
-        showChartValuesOutside: _showChartValuesOutside,
-      ),
-      ringStrokeWidth: _ringStrokeWidth!,
-      emptyColor: Colors.grey,
-    );
-
     return Scaffold(
+        drawer: sideDrawerLeft(),
         appBar: AppBar(
           title: const Text('Sample Code'),
         ),
-        body: Container(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                child: chart,
-                margin: EdgeInsets.symmetric(
-                  vertical: 32,
-                ),
-              ),
-              Flexible(
-                child: ListView.builder(
-                    padding: const EdgeInsets.all(8),
-                    itemCount: dataMap.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return TextButton(
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(colorList.elementAt(index)),
+        body: FutureBuilder(
+            future: pullData(),
+            builder: (context, AsyncSnapshot<String> snapshot) {
+              return Container(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 300,
+                      child: PieChart(
+                        key: ValueKey(key),
+                        dataMap: getDataMap(snapshot.toString()),
+                        animationDuration: Duration(milliseconds: 800),
+                        chartLegendSpacing: _chartLegendSpacing!,
+                        chartRadius: math.min(
+                            MediaQuery.of(context).size.width / 2, 400),
+                        colorList: colorList,
+                        initialAngleInDegree: 0,
+                        chartType: _chartType!,
+
+                        // legendLabels: _showLegendLabel ? legendLabels : {},
+
+                        chartValuesOptions: ChartValuesOptions(
+                          showChartValueBackground: _showChartValueBackground,
+                          showChartValues: _showChartValues,
+                          showChartValuesInPercentage:
+                          _showChartValuesInPercentage,
+                          showChartValuesOutside: _showChartValuesOutside,
                         ),
-                        onPressed: () {
-                          for (int i = 0; i < dataMap.length; ++i) {
-                            if (index == i) {
-                              buildNewPopup(
-                                  context,
-                                  '${dataMap.keys.elementAt(index)}',
-                                  activityList);
-                            }
-                          }
-                        },
-                        child: Center(
-                            child: Text('${dataMap.keys.elementAt(index)}')),
-                      );
-                    }),
-              ),
-            ],
-          ),
-        ));
-  }
-}
-
-//Map<String, double> readDataFromFile() {
-
-//return null;
-//}
-
-class CounterStorage {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/counter.txt');
-  }
-
-  Future<File> writeCounter(String counter) async {
-    final file = await _localFile;
-
-    // Write the file
-    return file.writeAsString('$counter');
+                        ringStrokeWidth: _ringStrokeWidth!,
+                        emptyColor: Colors.grey,
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 32,
+                      ),
+                    ),
+                    Flexible(
+                      child: ListView.builder(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: dataMap.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return TextButton(
+                              style: ButtonStyle(
+                                foregroundColor:
+                                MaterialStateProperty.all<Color>(
+                                    Colors.blue),
+                              ),
+                              onPressed: () {
+                                for (int i = 0; i < dataMap.length; ++i) {
+                                  if (index == i) {
+                                    print(index);
+                                    buildNewPopup(
+                                        context,
+                                        "2", //fix this
+                                        generateMoodActivityLists(snapshot.toString())
+                                            .elementAt(i)); //very inefficient
+                                  }
+                                }
+                              },
+                              child: Center(
+                                  child:
+                                  Text('${dataMap.keys.elementAt(index)}')),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              );
+            }));
   }
 
-  Future<String> readCounter() async {
-    try {
-      final file = await _localFile;
+  Map<String, double> getDataMap(String myString) {
+    int numOfGood = 0;
+    int numOfBad = 0;
+    int numOfGreat = 0;
+    int numOfMedium = 0;
 
-      // Read the file
-      final contents = await file.readAsString();
+    numOfGood = countInstance(myString, "Good");
+    numOfBad = countInstance(myString, "Bad");
+    numOfGreat = countInstance(myString, "Great");
+    numOfMedium = countInstance(myString, "Medium");
 
-      return contents;
-    } catch (e) {
-      // If encountering an error, return 0
-      return "Problem";
+    return <String, double>{
+      "Great": numOfGreat.toDouble(),
+      "Good": numOfGood.toDouble(),
+      "Medium": numOfMedium.toDouble(),
+      "Bad": numOfBad.toDouble(),
+    };
+  }
+
+  int countInstance(String data, String check) {
+    int count = 0;
+
+    for (int i = 0; i < data.length - check.length; ++i) {
+      if (data.substring(i, i + check.length) == check) {
+        count++;
+      }
     }
+    return count;
+  }
+
+  List<List<String>> generateMoodActivityLists(String data) {
+
+    String theString = "";
+    List<String> list1 = [];
+    List<String> list2= [];
+    List<String> list3= [];
+    List<String> list4= [];
+
+    print(data);
+
+
+    int j = 0;
+
+
+    for (int i = 0; i < data.length; ++i) {
+      if (data[i] == '{') {
+        while (data[i + j] != '}') {
+          ++j;
+          if (data[i + j] == '{') {
+
+            j=0;
+            break;
+          }
+
+        }
+
+        // print(data[i+j]);
+        if(data[i+j] == '}') {
+          // print(data.substring(i, i+j+1));
+
+          for(int k=i; k < i+j+1; ++k) {
+            if(data.substring(k, k+8) == "Activity") {
+              theString = data.substring(k+10, i+j);
+
+
+
+              if(data.substring(i, i+j+1).contains(": Great")) {
+                list1.add(theString);
+              } else if(data.substring(i,  i+j+1).contains(": Good")) {
+                list2.add(theString);
+              } else if(data.substring(i,  i+j+1).contains(": Medium")) {
+                list3.add(theString);
+              }
+              else if(data.substring(i,  i+j+1).contains(": Bad")) {
+
+                list4.add(theString);
+              }
+
+              break;
+            }
+          }
+
+        }
+        j=0;
+      }
+    }
+
+    List<List<String>> returnList = [];
+    returnList.add(list1);
+    returnList.add(list2);
+    returnList.add(list3);
+    returnList.add(list4);
+
+    return returnList;
   }
 }
